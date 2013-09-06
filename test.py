c172a6a82fa6956339670e38caa0025afb1540ce
Dave Kujawski <dave.kujawski@gmail.com>
2013-09-06 17:28:33 +1200

    adding tests for the findLongest function

--- test.py
+++ test.py
@@ -14,3 +14,37 @@ def test_isPalindrome_dave():
 @raises(palindrome.PalindromeException)
 def test_isPalindrome_empty():
 	assert_false( palindrome.isPalindrome("") )
+
+# -- 
+
+def test_findLargest_abba():
+	assert_equal( palindrome.findLargest("ABBA"), "ABBA" )
+
+def test_findLargest_abcba():
+	assert_equal( palindrome.findLargest("ABCBA"), "ABCBA" )
+
+def test_findLargest_dave():
+	assert_equal( palindrome.findLargest("DAVE"), None )
+
+@raises(palindrome.PalindromeException)
+def test_findLargest_empty():
+	assert_false( palindrome.findLargest("") )
+
+# --
+
+def test_findLargest_abbaxyzzyx():
+	assert_equal( palindrome.findLargest("ABBAXYZZYX"), "XYZZYX" )
+
+def test_findLargest_abcdefghijklmnopo():
+	assert_equal( palindrome.findLargest("ABCDEFGHIJKLMNOPO"), "OPO" )
+
+def test_findLargest_abcdefghijklmnopqrstuvwxyz():
+	assert_equal( palindrome.findLargest("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), None )
+
+def test_findLargest_aabcdefghijklmnopqrstuvwxyz():
+	assert_equal( palindrome.findLargest("AABCDEFGHIJKLMNOPQRSTUVWXYZ"), "AA" )
+
+def test_findLargest_example():
+	value = "GHDUFNEICJSODLFJGHFYDNDJSKDNCHFGEJDBDJEGFHCNDKYATRSMZPLOBSRQNSB"
+	assert_equal( palindrome.findLargest(value), "KDNCHFGEJDBDJEGFHCNDK" )
+
