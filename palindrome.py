4023ffb87994c77311aa2a7fa5738ca1c7646f9e
Dave Kujawski <dave.kujawski@gmail.com>
2013-09-06 17:28:04 +1200

    adding findPalindrome function to the module

--- palindrome.py
+++ palindrome.py
@@ -5,15 +5,25 @@ class PalindromeException(Exception):
 def isPalindrome(str_text):
 	n = len(str_text)
 	if n <= 0:
-		# is an empty string a palendrome?
-		# TODO, raise exception here instead of return False
 		raise PalindromeException("Empty string.")
 	# integer division handles odd len strings leaving the middle letter out
 	key = n/2
-	# get the first half of the string
-	comp_start = str_text[:key]
-	# get the last half
-	comp_end = str_text[-key:]
-	# compare the two strings, reverse the second
-	return comp_start == comp_end[::-1]
+	# compare the first half of the string with the reverse of the second
+	return str_text[:key] == str_text[-key:][::-1]
 
+def findLargest(str_text):
+	n = len(str_text)
+	if n <= 0:
+		raise PalindromeException("Empty string.")
+	# first slot to check is the full string
+	slot_len = n
+	while slot_len > 0:
+		n_slots = n - slot_len + 1 # zero counts as a slot
+		for pos in xrange(n_slots):
+			# shift the slot over the full length of the string
+			pos_end = slot_len+pos
+			token = str_text[pos:pos_end]
+			if isPalindrome(token):
+				return token
+		# shrink the slot for the next iteration
+		slot_len-=1		
