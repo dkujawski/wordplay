382cfb62dcea61cfc4c193a2dd7e0f44959c3320
Dave Kujawski <dave.kujawski@gmail.com>
2013-09-08 09:10:54 +1200

    first sketch of the traversal logic for the 2d matrix.

--- sketch.py
+++ sketch.py
@@ -49,6 +49,63 @@ def findLargest(str_text):
 				return token
 		slot_len-=1
 
+class Resident(object):
+	def __init__(self, value, address):
+		self.value = value
+		self.address = address
+		self.neighbors = list()
+	
+	def traverse(self, results, trail=""):
+		trail += self.value
+		for neighbor in self.neighbors:
+			neighbor.traverse(results, trail)
+		if len(self.neighbors) == 0:
+			results.append(trail)
+
+def findNeighbors(cell, array_2d, visited):
+	(x, y) = cell.address
+	visited.add(cell.address)
+	def _tryToAppendValue(_pos):
+		_x, _y = _pos
+		# pass on neg values as being outside array bounds
+		if _x < 0 or _y < 0:
+			return
+		# walk back through parents.
+		if _pos in visited:
+			# exclude any neighbors that are already in attendance
+			return
+		try:
+			value = array_2d[_x][_y]
+		except IndexError as ie:
+			# pass on any values outside of array bounds.
+			return
+		n = findNeighbors(Resident(value, (_x, _y), cell.address), array_2d, visited)
+		cell.neighbors.append(n)
+	# get horiz neighbors
+	_tryToAppendValue((x-1, y))
+	_tryToAppendValue((x+1, y))
+	# get vert neighbors
+	_tryToAppendValue((x, y-1))
+	_tryToAppendValue((x, y+1))
+	# get diags
+	_tryToAppendValue((x+1, y-1))
+	_tryToAppendValue((x+1, y+1))
+	_tryToAppendValue((x-1, y-1))
+	_tryToAppendValue((x-1, y+1))
+
+	return cell
+
+def walkTheSet(array_2d):
+	""" generate list of traversals
+	"""
+	results = list()
+	for x, x_val in enumerate(array_2d):
+		for y, y_val in enumerate(x_val):
+			# recursively dig to build the graph
+			cell = findNeighbors(Resident(array_2d[x][y], (x,y)), array_2d, set())
+			cell.traverse(results)
+	return results
+
 if __name__ == "__main__":
 	ch = logging.StreamHandler()
 	ch.setLevel(logging.DEBUG)
@@ -59,6 +116,13 @@ if __name__ == "__main__":
 	#print isPalindrome("ABCBA")
 	#print isPalindrome("DAVE")
 	
-	print findLargest("ABBAXYZZYX")
+	#print findLargest("ABBAXYZZYX")
 
-
+	tmp_values = [[ 'I', 'T', 'N', 'D', 'O' ],
+				  [ 'S', 'A', 'K', 'X', 'N' ],
+				  [ 'L', 'B', 'Z', 'M', 'E' ],
+				  [ 'P', 'E', 'L', 'M', 'E' ],
+				  [ 'O', 'P', 'L', 'S', 'T' ]]
+	
+	#print findNeighbors((2,2), tmp_values)
+	print walkTheSet(tmp_values)
