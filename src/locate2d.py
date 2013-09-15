c853e7b17f2be8aeeb8aab3c7ba27fee951f5791
Dave Kujawski <dave.kujawski@gmail.com>
2013-09-15 23:29:27 +1200

    adding doc strings

--- src/locate2d.py
+++ src/locate2d.py
@@ -1,25 +1,67 @@
+"""
+.. sectionauthor:: Dave Kujawski
+
+Here are a few functions for processing a 2 dimensional array or a set of
+nested lists like::
+
+	array_2d = [['A','B','C'],
+				['D','E','F'],
+				['G','H','I'],]
+
+This module uses the :class:`node.Node` class.
+
+"""
+
 from node import Node
 
 def build_graph(array_2d, pos=(0,0), nodes=None):
+	"""
+	Using the values from the :param:`array_2d` buid a graph/tree data 
+	structure and build a dict containing a :class:`node.Node` for each element 
+	of :param:`array_2d`.  This uses the :meth:`get_neighbors` to build node 
+	edges for the graph.
+
+	:param array_2d: A list of lists containing strings. 
+	:param pos: This is an internal parameter used in the recursive calls
+		to know which element of the :param:`array_2d` to start processing 
+		next.
+	:param nodes: This is a dict of :class:`node.Node` objects used internally
+		to keep track of which elements have already been processed and to 
+		prevent infinite loops in the recursion.  It takes the form::
+
+			{(x,y):node.Node, ...}
+
+		It is also an opportunity to get a list containing a :class:`node.Node` 
+		for each element in the :param:`array_2d` when an initialized dict is 
+		passed in.
+
+	"""
 	if nodes is None:
 		nodes = dict()
 	x,y = pos
 	if pos in nodes:
+		# if the node has already be built, reuse it
+		# no need for duplicate objects
 		node = nodes[pos]
 	else:
+		# this is the first time visiting this element
+		# create a node and add it to the dict.
 		value = array_2d[x][y]
 		node = Node(value, pos)
 		nodes[pos] = node
+	# go get the neighbors, these are edges in the graph
 	neighbors = get_neighbors(nodes, array_2d, pos)
+	# visit each neighbor to ensure it is a valid edge.
 	for neighbor in neighbors:
 		if neighbor in node.neighbors:
-			# we have already have this neighbor
+			# we have already have this edge
 			continue
 		node.neighbors.append(neighbor)
 		if node in neighbor.neighbors:
-			# avoid infinite loops
+			# avoid infinite loops, we are an edge of this neighbor
 			continue
 		else:
+			# recurse
 			build_graph(array_2d, neighbor.address, nodes)
 
 def get_neighbors(nodes, array_2d, root_pos):
