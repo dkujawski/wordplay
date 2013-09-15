2f05ec272d33e69515902aa6918551f5223b4faa
Dave Kujawski <dave.kujawski@gmail.com>
2013-09-15 23:05:01 +1200

    adding doc strings. adding t_val and h_val to make code more readable to explicitly describe that params are meant to be passed by value.

--- src/node.py
+++ src/node.py
@@ -12,6 +12,16 @@ class Node(object):
 	"""
 	This is a simple node class used for constructing a graph or tree data 
 	structure.
+
+	Example usage::
+
+		a = node.Node('A', (0,0))
+		b = node.Node('B', (0,1))
+		c = node.Node('C', (1,0))
+		a.neighbors.append(b)
+		b.neighbors.append(c)
+		results = a.traverse()
+
 	"""
 	def __init__(self, value, address):
 		self.value = value
@@ -19,8 +29,30 @@ class Node(object):
 		self.neighbors = list()
 
 	def traverse(self, func=None, results_ref=None, trail=None, history=None):
-		""" walk the tree building a string characters from each 
-		value.
+		""" 
+		Traverse the graph building a trail of values as each node is visited.
+		Update the result list with the concatonated string values when a leaf
+		node is reached.
+
+		:param func: a function pointer to a function that returns True or
+			False and accepts a string as a the single parameter.  If present
+			this function will be called using the concatonated string
+			found at the leaf node. If the result is True the string will be 
+			added to the result list.
+		:param result_ref: This is initialized internally at the start of a 
+			traversal.  This is the main container passed by ref through the
+			recursive calls and returned at the end.
+		:param trail: This is initialized internally at the start of the
+			traversal.  This is the main list used to collect the node values
+			from each node as they are visited.  When a leaf node is reached
+			the list is joined to build a string of values and added to the 
+			:param:``result_ref``.
+		:param history: This is an internal value used during recursive
+			traversals to track where we have already been to avoid
+			duplicate values and infinite loops.
+		:returns: list containing the collected concatonated strings found
+			during the traversal.
+
 		"""
 		if results_ref is None:
 			results_ref = list()
@@ -36,9 +68,15 @@ class Node(object):
 		for neighbor in self.neighbors:
 			if neighbor.address in history:
 				continue
+			# building copy of these lists to pass by value, we want each 
+			# iteration of the loop to use its own history.  if passed by
+			# ref subsequent loop iterations' history would be contaminated
+			# with history from an unrelated traversal.
+			t_val = list(trail)
+			h_val = list(history)
 			# recurse for all neighbors
-			results_ref = neighbor.traverse(func, results_ref, 
-											list(trail), list(history))
+			results_ref = neighbor.traverse(func, results_ref, t_val, h_val)
+
 		trail_str = ''.join(trail)
 	   	if not results_ref or not results_ref[-1].startswith(trail_str):
 			if func is None or func(trail_str):
