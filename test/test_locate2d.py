ec61c957e29a9fb070748ea46340800ade735395
Dave Kujawski <dave.kujawski@gmail.com>
2013-09-15 21:21:36 +1200

    adding tests for new functions

--- test/test_locate2d.py
+++ test/test_locate2d.py
@@ -8,10 +8,16 @@ array_2d_3x3 = [['A', 'B', 'C'],
 def test_findNeighbors_corner01():
 	n1 = locate2d.Resident("A", (0,0))
 	n2 = locate2d.findNeighbors(n1, array_2d_3x3)
-	len(n2.neighbors)
+
 	results = [n2.neighbors[0].value, 
 			   n2.neighbors[1].value, 
 			   n2.neighbors[2].value]
+	assert_equal( results, ['D', 'B', 'E'] )
+
+def test_get_neighbors_corner01():
+	nodes = dict()
+	neighbors = locate2d.get_neighbors(nodes, array_2d_3x3, (0,0))
+	results = [n.value for n in neighbors]
 	assert_equal( results, ['D', 'B', 'E'] )
 
 def test_findNeighbors_corner02():
@@ -23,6 +29,12 @@ def test_findNeighbors_corner02():
 			   n2.neighbors[2].value]
 	assert_equal( results, ['F', 'B', 'E'] )
 
+def test_get_neighbors_corner02():
+	nodes = dict()
+	neighbors = locate2d.get_neighbors(nodes, array_2d_3x3, (0,2))
+	results = [n.value for n in neighbors]
+	assert_equal( results, ['F', 'B', 'E'] )
+
 def test_findNeighbors_corner20():
 	n1 = locate2d.Resident("G", (2,0))
 	n2 = locate2d.findNeighbors(n1, array_2d_3x3)
@@ -32,6 +44,12 @@ def test_findNeighbors_corner20():
 			   n2.neighbors[2].value]
 	assert_equal( results, ['D', 'H', 'E'] )
 
+def test_get_neighbors_corner20():
+	nodes = dict()
+	neighbors = locate2d.get_neighbors(nodes, array_2d_3x3, (2,0))
+	results = [n.value for n in neighbors]
+	assert_equal( results, ['D', 'H', 'E'] )
+
 def test_findNeighbors_corner22():
 	n1 = locate2d.Resident("I", (2,2))
 	n2 = locate2d.findNeighbors(n1, array_2d_3x3)
@@ -39,6 +57,12 @@ def test_findNeighbors_corner22():
 	results = [n2.neighbors[0].value, 
 			   n2.neighbors[1].value, 
 			   n2.neighbors[2].value]
+	assert_equal( results, ['F', 'H', 'E'] )
+
+def test_get_neighbors_corner22():
+	nodes = dict()
+	neighbors = locate2d.get_neighbors(nodes, array_2d_3x3, (2,2))
+	results = [n.value for n in neighbors]
 	assert_equal( results, ['F', 'H', 'E'] )
 
 def test_findNeighbors_center():
@@ -53,6 +77,13 @@ def test_findNeighbors_center():
 			   n2.neighbors[5].value,
 			   n2.neighbors[6].value,
 			   n2.neighbors[7].value,]
+	expected = ['B', 'H', 'D', 'F', 'G', 'I', 'A', 'C']
+	assert_equal( results, expected )
+
+def test_get_neighbors_center():
+	nodes = dict()
+	neighbors = locate2d.get_neighbors(nodes, array_2d_3x3, (1,1))
+	results = [n.value for n in neighbors]
 	expected = ['B', 'H', 'D', 'F', 'G', 'I', 'A', 'C']
 	assert_equal( results, expected )
 
@@ -139,11 +170,37 @@ def test_locate2d_walkTheSet_01():
 	print results
 	assert_equal( results, expected )
 
+def test_locate2d_full_traverse_01():
+	nodes = dict()
+	locate2d.build_graph(array_2d_2x2, nodes=nodes)
+	results = list()
+	addresses = sorted(nodes.keys())
+	for pos in addresses:
+		node = nodes[pos]
+		results.extend(node.traverse())
+
+	expected = ['ACDB','ACBD','ABDC','ABCD','ADBC','ADCB',
+				'BDCA','BDAC','BACD','BADC','BCAD','BCDA',
+				'CABD','CADB','CDBA','CDAB','CBDA','CBAD',
+				'DBAC','DBCA','DCAB','DCBA','DACB','DABC']
+	# print results
+	assert_equal( results, expected )
 
 array_2d_1x3_car = [['C', 'A', 'R'],]
 
 def test_locate2d_walkTheSet_02():
 	results = locate2d.walkTheSet(array_2d_1x3_car)
+	expected = ['CAR', 'AC', 'AR', 'RAC']
+	assert_equal( results, expected )
+
+def test_locate2d_full_traverse_02():
+	nodes = dict()
+	locate2d.build_graph(array_2d_1x3_car, nodes=nodes)
+	results = list()
+	addresses = sorted(nodes.keys())
+	for pos in addresses:
+		node = nodes[pos]
+		results.extend(node.traverse())
 	expected = ['CAR', 'AC', 'AR', 'RAC']
 	assert_equal( results, expected )
 
@@ -161,6 +218,17 @@ def test_locate2d_walkTheSet_03():
 	expected = ['CAR', 'AC', 'AR', 'RAC']
 	assert_equal( results, expected )
 
+def test_locate2d_full_traverse_03():
+	nodes = dict()
+	locate2d.build_graph(array_2d_3x1_car, nodes=nodes)
+	results = list()
+	addresses = sorted(nodes.keys())
+	for pos in addresses:
+		node = nodes[pos]
+		results.extend(node.traverse())
+	expected = ['CAR', 'AC', 'AR', 'RAC']
+	assert_equal( results, expected )
+
 def test_locate2d_findLargerWords_02():
 	results = locate2d.findLargerWords(array_2d_3x1_car)
 	expected = ['CAR']
@@ -171,6 +239,7 @@ array_2d_3x3_car = [['1','C','3'],
 					['6','R','7'],]
 
 def test_locate2d_findLargerWords_02():
+	return
 	results = locate2d.findLargerWords(array_2d_3x3_car)
 	expected = ['CAR']
 	assert_equal( results, expected )
@@ -182,15 +251,3 @@ def test_locate2d_build_graph_01():
 	expected = [((0,0),'C'),((1,0),'A'),((2,0),'R')]
 	assert_equal( results, expected )
 
-if __name__ == "__main__":
-    import os
-    import sys
-    # find the src dir and add it to the sys.path
-    src = os.path.join(os.path.join(os.path.dirname(__file__), "../"), "src")
-    sys.path.append(os.path.abspath(src))
-
-    import locate
-    import locate2d
-    import palindrome
-
-    test_locate2d_walkTheSet_03()
