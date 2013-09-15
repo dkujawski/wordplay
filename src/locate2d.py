7338b999ca72fe24891fc04492cd4dc62061103d
Dave Kujawski <dave.kujawski@gmail.com>
2013-09-15 23:39:41 +1200

    adding doc strings

--- src/locate2d.py
+++ src/locate2d.py
@@ -65,8 +65,26 @@ def build_graph(array_2d, pos=(0,0), nodes=None):
 			build_graph(array_2d, neighbor.address, nodes)
 
 def get_neighbors(nodes, array_2d, root_pos):
+	""" 
+	Starting from the element at :param:`root_pos` find all adjacent elements
+	in the :param:`array_2d`, create a :class:`node.Node` for each, add them to 
+	the :param:`nodes` dict and return a list which represent edges between the
+	:param:`root_pos` node.
+
+	:param nodes: This is the passed by ref nodes dict from :meth:`build_graph`.  
+		This is passed in to avoid creating dupliecate :class:`node.Node` 
+		objects.  If they already exist in the :param:`nodes` dict we will
+		resuse them.
+	:param array_2d: This is the list of lists passed in from 
+		:meth:`build_graph` to search for neighbors.
+	:param root_pos: This is the element (x,y) that we are using as the point
+		of origin to search for valid edges.
+	:returns: List of :class:`node.Node` objects that are adjacent to the 
+		:param:`root_pos` in the :param:`array_2d` lists.
+
+	"""
 	x,y = root_pos	
-	# build address map
+	# build address search map
 	addresses = ((x-1,y), (x+1,y),
 				 (x,y-1), (x,y+1),
 				 (x+1,y-1), (x+1,y+1),
@@ -78,6 +96,7 @@ def get_neighbors(nodes, array_2d, root_pos):
 			# pass on negative coords
 			continue
 		if pos in nodes:
+			# if a node.Node already exists use it
 			node = nodes[pos]
 		else:
 			try:
@@ -86,7 +105,9 @@ def get_neighbors(nodes, array_2d, root_pos):
 				# pass on any values outside of array bounds.
 				continue
 			node = Node(value, pos)
+			# new nodes get added to the nodes dict for use later
 			nodes[pos] = node
+		# collect edges
 		neighbors.append(node)
 	return neighbors
 
